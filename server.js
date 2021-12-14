4d0cdfdbd09eff711305a421afb769e74cc73b87
Thiago Silva Pereira <thiagoufma@gmail.com>
2021-12-13 23:14:22 -0300

    Foreach

--- server.js
+++ server.js
@@ -6,7 +6,37 @@ app.set("view engine", "ejs");
 
 //rota index
 app.get("/", function (req, res) {
-  res.render("pages/index");
+  const items = [
+    {
+      title: "D",
+      message: "Desenvolvendo aplicações/serviços de forma fácil"
+    },
+    {
+      title: "E",
+      message: "EJS usa JavaScript para renderizar HTML"
+    },
+    {
+      title: "M",
+      message: "Muito Fácil de Usar"
+    },
+    {
+      title: "A",
+      message: "Ajuda demais"
+    },
+    {
+      title: "I",
+      message: "install ejs"
+    },
+    {
+      title: "S",
+      message: "Salvador"
+    },
+  ];
+  const subtitle = 'Uma linguagem de modelagem para criação de página HTML utilizando JavaScript';
+  res.render("pages/index", {
+    qualitys: items,
+    subtitle: subtitle,
+  });
 });
 
 //rota sobre
